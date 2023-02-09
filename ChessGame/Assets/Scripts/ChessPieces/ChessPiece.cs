e0757cf95da8ff4a47b6b4e0df2644635ec6caef
Alexey Kireyshin <alexey.kireyshin@swagmasha.org>
2023-02-09 14:52:41 +0300

    Basic class of chess pieces

---
+++ ChessGame/Assets/Scripts/ChessPieces/ChessPiece.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public enum ChessPieceType
+{
+    None = 0,
+    Pawn = 1,
+    Rook = 2,
+    Knight = 3,
+    Bishop = 4,
+    Qeen = 5,
+    King = 6
+}
+
+public class ChessPiece : MonoBehaviour
+{
+    public int team;
+    public int currentX;
+    public int currentY;
+    public ChessPieceType chessPieceType;
+
+    private Vector3 _desiredPosition;
+    private Vector3 _desiredScale;
+}
